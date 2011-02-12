5a0af32f70565f8e016a358c0ed3f288e290e4c9
Victor Bilyk <victorbilyk@gmail.com>
2011-02-12 17:02:53 +0600

    just a plain sbt project with default publish locations

--- project/build/ScalaJsonProject.scala
+++ project/build/ScalaJsonProject.scala
@@ -1,13 +1,10 @@
 import sbt._
-import com.twitter.sbt._
 
-class ScalaJsonProject(info: ProjectInfo) extends StandardProject(info) with SubversionPublisher {
+class ScalaJsonProject(info: ProjectInfo) extends DefaultProject(info) {
   val specs = buildScalaVersion match {
     case "2.7.7" => "org.scala-tools.testing" % "specs" % "1.6.2.1" % "test"
     case _ =>       "org.scala-tools.testing" % "specs_2.8.1" % "1.6.6" % "test"
   }
-
-  override def subversionRepository = Some("http://svn.local.twitter.com/maven-public/")
 
   override def disableCrossPaths = false
 
